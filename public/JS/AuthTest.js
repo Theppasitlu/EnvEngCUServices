4f79d57473da2600eb3c51d09e119b0605f07aac
EnvStaff <EnvStaff@gmail.com>
2023-08-17 18:46:41 +0700

    เกือบซ่อนได้

--- public/JS/AuthTest.js
+++ public/JS/AuthTest.js
@@ -1,3 +1,15 @@
+import '../CSS/theme.css';
+import { 
+    hideLoginError, 
+    showLoginState, 
+    showLoginForm, 
+    showApp, 
+    showLoginError, 
+    btnLogin,
+    btnSignup,
+    btnLogout
+  } from './ui.js'
+
 import { initializeApp } from "firebase/app";
 import { 
     getAuth,
@@ -7,7 +19,7 @@ import {
     // signInWithEmailAndPassword,
     GoogleAuthProvider, 
     signInWithPopup, 
-    connectAuthEmulator,
+    // connectAuthEmulator,
     } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -32,31 +44,88 @@ connectAuthEmulator(auth, "http://localhost:9099");
 // const snapshot = await getDocs(todosCol);
 const provider = new GoogleAuthProvider();
 
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+// signInWithPopup(auth, provider)
+//   .then((result) => {
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     const credential = GoogleAuthProvider.credentialFromResult(result);
+//     const token = credential.accessToken;
+//     // The signed-in user info.
+//     const user = result.user;
+//     // IdP data available using getAdditionalUserInfo(result)
+//     // ...
+//   }).catch((error) => {
+//     // Handle Errors here.
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // The email of the user's account used.
+//     const email = error.customData.email;
+//     // The AuthCredential type that was used.
+//     const credential = GoogleAuthProvider.credentialFromError(error);
+//     // ...
+//   });
 
 signOut(auth).then(() => {
     // Sign-out successful.
   }).catch((error) => {
     // An error happened.
   });
+
+  const loginEmailPassword = async () => {
+    const loginEmail = txtEmail.value;
+    const loginPassword = txtPassword.value;
+
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+        console.log(userCredential.user);
+    }
+    catch (error) {
+        console.log(error);
+        showLoginError(error);
+        }
+}
+btnLogin.addEventListener("click", loginEmailPassword);
+
+// Create new account using email/password
+const createAccount = async () => {
+    const email = txtEmail.value
+    const password = txtPassword.value
+  
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(userCredential.user);
+    }
+    catch(error) {
+      console.log(`There was an error: ${error}`)
+      showLoginError(error)
+    } 
+  }
+btnSignup.addEventListener("click", createAccount)
+
+// Monitor auth state
+const monitorAuthState = async () => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        console.log(user)
+        showApp()
+        showLoginState(user)
+  
+        hideLoginError()
+        hideLinkError()
+      }
+      else {
+        showLoginForm()
+        lblAuthState.innerHTML = `You're not logged in.`
+      }
+    });
+  }
+  
+  // Log out
+  const logout = async () => {
+    await signOut(auth);
+  }
+btnLogout.addEventListener("click", logout)
+
+monitorAuthState();
 
 // ตรวจสอบสิทธิ์การเข้าถึง
 onAuthStateChanged(auth, user => {
